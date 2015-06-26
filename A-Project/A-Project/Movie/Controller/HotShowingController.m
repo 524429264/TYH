e7feeb9573d887b334f391f64d39ae055db04f74
lanou3g <524429264@qq.com>
2015-06-26 11:43:30 +0800

    kURL

--- A-Project/A-Project/Movie/Controller/HotShowingController.m
+++ A-Project/A-Project/Movie/Controller/HotShowingController.m
@@ -138,7 +138,7 @@
     return cell;
 }
 
-#define
+#define kURL1 @"ew"
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
